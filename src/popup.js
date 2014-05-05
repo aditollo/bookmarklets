ccef900aaa5177a1e89cae784894ca6831ef956c
aditollo <alfonso.ditollo@gmail.com>
2014-05-05 13:53:38 +0200

    fix per jshint e build

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,13 @@
 (function (){
 	"use strict";
 
-   $('.button[data-greeting]').on('click', function(){
-	   var dataGreeting = $(this).data('greeting');
-	   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		   chrome.tabs.sendMessage(tabs[0].id, {greeting: dataGreeting}, function(response) {
-			   console.log(response.farewell);
-		   });
-	   });
-   });
+	$('.button[data-greeting]').on('click', function(){
+		var dataGreeting = $(this).data('greeting');
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {greeting: dataGreeting}, function(response) {
+				console.log(response.farewell);
+			});
+		});
+	});
 
 })();

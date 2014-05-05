5ccb7c7be42ed05b82f434439b43a21ebbf7b3fd
aditollo <alfonso.ditollo@gmail.com>
2014-05-05 12:02:24 +0200

    toggleDevice in tutti i siti

--- src/content.js
+++ src/content.js
@@ -9,6 +9,7 @@
 			}
 		},
 		addItemToCart: function(){
+//			TODO Generalizzare la funzione per i vari siti
 			_$Y.watch('ECPack.search:SearchFull', function(data){
 				var items = data.Results.Items;
 				var item = items[Math.floor(Math.random()*items.length)];
@@ -24,23 +25,25 @@
 			});
 		},
 		openTimedLayer: function(){
+			//TODO testare
 //			%7Bfunction%20deleteSession()%7Bvar%20timeToReplace%3Bvar%20myooxTime%20%3D%20_%24Y.cookie.get('MYOOX'%2C%20'TIME')%3Bvar%20accountTime%20%3D%20_%24Y.cookie.get('ACCOUNT'%2C%20'TIME')%3Bif%20(myooxTime)%20%7BtimeToReplace%20%3D%20myooxTime.substring(0%2C%20myooxTime.length-2)%20%2B%20'00'%3B_%24Y.cookie.setProps('MYOOX'%2C%20%7B%20'TIME'%20%3A%20timeToReplace%20%7D)%3B%7Dif%20(accountTime)%20%7BtimeToReplace%20%3D%20accountTime.substring(0%2C%20accountTime.length-2)%20%2B%20'00'%3B_%24Y.cookie.setProps('ACCOUNT'%2C%20%7B%20'TIME'%20%3A%20timeToReplace%20%7D)%3B%7D%7DdeleteSession()%7D
-					var timeToReplace;
-					var myooxTime = _$Y.cookie.get('MYOOX', 'TIME');
-					var accountTime = _$Y.cookie.get('ACCOUNT', 'TIME');
-					if (myooxTime) {
-						timeToReplace = myooxTime.substring(0, myooxTime.length-2) + '00';
-						_$Y.cookie.setProps('MYOOX', { 'TIME' : timeToReplace });
-					}
-					if (accountTime) {
-						timeToReplace = accountTime.substring(0, accountTime.length-2) + '00';
-						_$Y.cookie.setProps('ACCOUNT', { 'TIME' : timeToReplace });
-					}
+			var timeToReplace;
+			var myooxTime = _$Y.cookie.get('MYOOX', 'TIME');
+			var accountTime = _$Y.cookie.get('ACCOUNT', 'TIME');
+			if (myooxTime) {
+				timeToReplace = myooxTime.substring(0, myooxTime.length-2) + '00';
+				_$Y.cookie.setProps('MYOOX', { 'TIME' : timeToReplace });
+			}
+			if (accountTime) {
+				timeToReplace = accountTime.substring(0, accountTime.length-2) + '00';
+				_$Y.cookie.setProps('ACCOUNT', { 'TIME' : timeToReplace });
+			}
 		},
 		delNewsLetterLayerCookie: function(){
 			_$Y.cookie.del('LAYER_NL');
 		},
 		addAddressBook: function(){
+			//TODO testare per cina e giappone
 			$.get('http://randomuser.me/g/', function(data) {
 				ECPack.account.addUserAddressBook({
 					address: 'via di qua',
@@ -64,10 +67,12 @@
 				$('body').attr('contenteditable', true);
 			}
 		},
-			lidChecker: function(){
+		lidChecker: function(){
+			//TODO dismettere
 			$("a:not([name^='&lid'])").css('border', "1px solid red");
 		},
 		marker: function(){
+			//TODO dismettere
 			var div = document.createElement("div");
 			div.id = "markerRecap";
 			document.body.insertBefore(div, document.body.firstChild);
@@ -106,11 +111,30 @@
 			});
 		},
 		toggleDevice: function(){
-			if(_$Y.cookie.get('RESOURCEINFO').indexOf('DEVICE=smartphone') > -1) {
+			//TODO Testare
+			//RESOURCEINFO basta per thecorner e shoescribe. Per gli altri siti servono anche MOBILEINFO e UI
+			if((_$Y.cookie.get('RESOURCEINFO') !== null && _$Y.cookie.get('RESOURCEINFO').indexOf('DEVICE=smartphone') > -1)
+				|| (_$Y.cookie.get('MOBILEINFO') !== null && _$Y.cookie.get('MOBILEINFO').indexOf('MOBILE=1') > -1)) {
 				_$Y.cookie.del('RESOURCEINFO');
+				_$Y.cookie.del('MOBILEINFO');
+				if(_$Y.cookie.get('UI') !== null)
+					_$Y.cookie.set('UI',_$Y.cookie.get('UI').replace('DEVICE=smartphone','DEVICE=desktop'));
 			}
 			else {
-				javascript:_$Y.cookie.set('RESOURCEINFO', 'DEVICE=smartphone&ORIGINALDEVICE=smartphone', 365);
+				_$Y.cookie.set('RESOURCEINFO', 'DEVICE=smartphone&ORIGINALDEVICE=smartphone', 365);
+				_$Y.cookie.set('MOBILEINFO', 'MOBILE=1', 365);
+				if(_$Y.cookie.get('UI') !== null)
+					_$Y.cookie.set('UI',_$Y.cookie.get('UI').replace('DEVICE=desktop','DEVICE=smartphone'), 365);
+			}
+			location.reload();
+		},
+		toggleSrcMode: function(){
+			//TODO Testare
+			if(_$Y.cookie.get('SRCMODE') !== null) {
+				_$Y.cookie.del('SRCMODE');
+			}
+			else {
+				javascript:_$Y.cookie.set('SRCMODE', '1', 365);
 			}
 			location.reload();
 		}
